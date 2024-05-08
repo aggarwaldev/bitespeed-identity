046b1916159d210ccdd93b9bbf0b2b7ba64dbf06
Dev Aggarwal <22045791+aggarwaldev@users.noreply.github.com>
2024-05-08 13:28:24 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  plugins: ['@typescript-eslint', 'node', 'prettier'],
+  extends: ['airbnb-base'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended-type-checked',
+      ],
+      rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'import/prefer-default-export': 'off',
+        'class-methods-use-this': 'off',
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: ['**/*spec.ts'] },
+        ],
+      },
+      parserOptions: {
+        project: true,
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    {
+      files: ['*.js', '*.ts', '*.tsx'],
+      extends: ['prettier'],
+      rules: {
+        'prettier/prettier': 'error',
+      },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,30 @@
+# Nest Template
+
+
+## Checklist
+- [ ] [Add DB Driver](#how-to-install-db-driver)
+
+
+
+### How to install DB Driver
+From [typeorm docs](https://typeorm.io/#installation):
+
+- for MySQL or MariaDB
+
+`yarn add mysql2`
+
+- for PostgreSQL or CockroachDB
+
+`yarn add pg`
+
+- for SQLite
+
+`yarn add sqlite3`
+
+- for Microsoft SQL Server
+
+`yarn add mssql`
+
+- for sql.js
+
+`yarn add sql.js`
