99930dcca6dcd77acce25ab31bcca1a0a40ecad7
Dev Aggarwal <contact@aggarwal.dev>
2024-05-08 15:49:15 +0530

    feat: swagger documentation
    (cherry picked from commit 801394288846f2fdea0aabffdec2988e2c058c79)

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { BackendModule } from './backend.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BackendModule);
 
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('BiteSpeed')
+    .setDescription('Identity Recon API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get<string>('PORT'));
 }
