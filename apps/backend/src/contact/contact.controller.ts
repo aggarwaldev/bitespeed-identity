c1cb863342385c0a29bf61704130f95d9368a626
Dev Aggarwal <contact@aggarwal.dev>
2024-05-08 18:09:43 +0530

    feat: logging in controller
    (cherry picked from commit 8e88d523049c5859328e726aacbbd410ce8d3b8f)

--- apps/backend/src/contact/contact.controller.ts
+++ apps/backend/src/contact/contact.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 
 import { Validate } from '@app/utils';
 
@@ -10,20 +10,29 @@ import { identifySchema } from './schema';
 
 @Controller()
 export class ContactController {
+  private readonly logger = new Logger(ContactController.name);
+
   constructor(public contactService: ContactService) {}
 
   @Validate(identifySchema)
   @Post('identify')
   async identify(@Body() contact: ContactDto) {
+    this.logger.log('Request received', contact);
+
     const results = await this.contactService.find(contact);
 
+    this.logger.verbose(`Fetched ${results.contacts.length} records`);
     // No existing records found, let's create one and do early return.
     if (!results.contacts.length) {
+      this.logger.log(`Creating a new contact`);
       await this.contactService.create(contact);
+
+      this.logger.verbose('New contact created');
 
       return this.respond(contact);
     }
 
+    this.logger.verbose('Finding existing records');
     const existingRecord = results.contacts.find(
       (r) =>
         // There could be a scenario where email/phone is not defined in request
@@ -32,15 +41,24 @@ export class ContactController {
           r.phoneNumber === contact.phoneNumber)
     );
 
+    this.logger.log(`Existing record found?`, existingRecord);
     if (!existingRecord) {
       // Request contains new information, we should create a new record.
+      this.logger.log('Creating a new record based on request...');
       await this.contactService.create(contact);
+      this.logger.verbose('New contact created');
     }
 
+    this.logger.verbose('Re-fetching contacts.');
     const { primaryContact, secondaryContact } =
       await this.contactService.fetchContacts(contact);
 
+    this.logger.log('Found contacts: ', { primaryContact, secondaryContact });
+
     if (secondaryContact.length) {
+      this.logger.verbose(
+        `There are ${secondaryContact.length} secondary contacts`
+      );
       const outDatedEntries = secondaryContact
         .filter(
           (r) =>
@@ -49,11 +67,17 @@ export class ContactController {
         )
         .map((r) => r.id);
 
-      if (outDatedEntries.length)
+      this.logger.verbose(
+        `There are ${outDatedEntries.length} secondary contacts that needs to be updated`
+      );
+
+      if (outDatedEntries.length) {
         await this.contactService.update(outDatedEntries, {
           linkedId: primaryContact.id,
           linkPrecedence: LinkPrecedence.SECONDARY,
         });
+        this.logger.log('Secondary contact(s) updated', outDatedEntries);
+      }
     }
 
     return this.respond(contact);
