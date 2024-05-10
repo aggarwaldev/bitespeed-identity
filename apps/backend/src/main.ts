0d498081231f8e44bffae24628a17235af3ba423
Dev Aggarwal <contact@aggarwal.dev>
2024-05-11 02:59:18 +0530

    fix: remove api versioning to match requirements

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { VersioningType } from '@nestjs/common';
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { BackendModule } from './backend.module';
@@ -8,17 +7,11 @@ import { BackendModule } from './backend.module';
 async function bootstrap() {
   const app = await NestFactory.create(BackendModule);
 
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
-
   const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
     .setTitle('BiteSpeed')
     .setDescription('Identity Recon API')
-    .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
